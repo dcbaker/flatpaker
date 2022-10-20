bf639635e2515a3149904199e1adf0e0dd8ca9df
Dylan Baker <dylan@pnwbakers.com>
2022-10-20 10:18:50 -0700

    fix compiling patches
    patches must be part of the initial command, as flatpak doesn't allow
    modifying sources from other steps, and the entire program must be
    recompiled.

--- renpy2flatpak.py
+++ renpy2flatpak.py
@@ -21,6 +21,7 @@ if typing.TYPE_CHECKING:
         domain: str
         name: str
         patches: typing.Optional[typing.List[str]]
+        install: bool
 
 
 def create_desktop(args: Arguments, workdir: pathlib.Path, appid: str) -> pathlib.Path:
@@ -56,13 +57,28 @@ def dump_yaml(args: Arguments, workdir: pathlib.Path, appid: str, desktop_file: 
             'build-commands': [
                 'mkdir -p /app/lib/game',
                 'cp -R * /app/lib/game',
-                'rm -f /app/lib/game/*.exe',
-                'rm -Rf /app/lib/game/*.app',
-                'rm -Rf /app/lib/game/lib/*-darwin-*',
-                'rm -Rf /app/lib/game/lib/*-windows-*',
-                'rm -Rf /app/lib/game/lib/*-i686',
                 # Patch the game to not require sandbox access
                 '''sed -i 's@"~/.renpy/"@os.environ.get("XDG_DATA_HOME", "~/.local/share") + "/"@g' /app/lib/game/*.py''',
+
+                'cp *.rpy /app/lib/game/game/',
+
+                # Compile the patch files.
+                'pushd /app/lib/game; ./*.sh . compile --keep-orphan-rpyc; popd',
+
+                # Remove the rpy files, which saves space
+                'rm /app/lib/game/game/*.rpy',
+            ],
+            'cleanup': [
+                '*.exe',
+                '*.app',
+                '*.rpyc.bak',
+            ],
+        },
+        {
+            'buildsystem': 'simple',
+            'name': 'game_sh',
+            'sources': [],
+            'build-commands': [
                 'mkdir -p /app/bin',
                 'echo  \'cd /app/lib/game/; export RENPY_PERFORMANCE_TEST=0; sh *.sh\' > /app/bin/game.sh',
                 'chmod +x /app/bin/game.sh'
@@ -94,25 +110,7 @@ def dump_yaml(args: Arguments, workdir: pathlib.Path, appid: str, desktop_file: 
                 'sha256': sha256(patch),
                 'type': 'file',
             })
-        modules.append({
-            'buildsystem': 'simple',
-            'name': 'patches',
-            'sources': sources,
-            'build-commands': [
-                'cp *.rpy /app/lib/game/game/',
-
-                # Compile the patch files. Existing files will not be
-                # recompiled, and will result in warnings to the console we want
-                # to suppress
-                'pushd /app/lib/game; ./*.sh . compile 2&>/dev/null; popd',
-
-                # Remove the rpy files, which saves space
-                'rm /app/lib/game/game/*.rpy',
-            ],
-            'cleanup': [
-                '*.rpyc.bak',
-            ]
-        })
+        modules[0]['sources'].extend(sources)
 
     struct = {
         'sdk': 'org.freedesktop.Sdk',
@@ -130,7 +128,12 @@ def dump_yaml(args: Arguments, workdir: pathlib.Path, appid: str, desktop_file: 
             '--socket=x11',
             '--device=dri',
         ],
-        'modules': modules
+        'modules': modules,
+        'cleanup-commands': [
+            'rm -Rf /app/lib/game/lib/*-darwin-*',
+            'rm -Rf /app/lib/game/lib/*-windows-*',
+            'rm -Rf /app/lib/game/lib/*-i686',
+        ],
     }
 
     with (pathlib.Path(workdir) / f'{appid}.yaml').open('w') as f:
@@ -145,6 +148,8 @@ def build_flatpak(args: Arguments, workdir: pathlib.Path, appid: str) -> None:
 
     if args.repo:
         build_command.extend(['--repo', args.repo])
+    if args.install:
+        build_command.extend(['--user', '--install'])
 
     subprocess.run(build_command)
 
@@ -156,6 +161,7 @@ def main() -> None:
     parser.add_argument('--domain', action='store', required=True, help="the reversed domain without the name. ex: 'com.github.user'")
     parser.add_argument('--name', action='store', required=True, help="the name of the project, without spaces")
     parser.add_argument('--patches', action='append', default=[], help="Additional rpy files to copy into the game folder")
+    parser.add_argument('--install', action='store_true', default=[], help="Install for the user (useful for testing)")
     args: Arguments = parser.parse_args()
 
     appid = f"{args.domain}.{args.name.replace(' ', '_')}"
