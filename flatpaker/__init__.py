8616a74335bf200937d5e73346f0b12e5791bfe7
Dylan Baker <dylan@pnwbakers.com>
2024-11-21 11:37:37 -0800

    use flit instead of setuptools

--- flatpaker/__init__.py
+++ flatpaker/__init__.py
@@ -1,3 +1,13 @@
 # SPDX-License-Identifier: MIT
 # Copyright © 2024 Dylan Baker
 
+"""Utilities to convert various kinds of binaries into flatpaks
+
+Current support includes Ren'Py and some versions of RPGMaker (MV and MZ, when
+they have Linux packages).
+
+Attempts to make some optimizations of the packages, such as recompiling
+bytecode.
+"""
+
+__version__ = "0.0.1"
