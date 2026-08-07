6e9839b7e0d3704bd38493a0cf02a4744b517cd2
Dylan Baker <dylan@pnwbakers.com>
2026-08-07 17:07:44 +0000

    devcontainer: Add basic devcontainer setup
    This requires a bunch of permissions in order to get bubblewrap working inside
    the container.
    
    It also adds flat-manager-client, which I plan to integrate into flatpaker
    in the near future as an export target.

---
+++ .devcontainer/debian-install.sh
@@ -0,0 +1,30 @@
+set -exo pipefail
+
+apt update -y
+
+apt install -y \
+    python3 \
+    appstream \
+    flatpak-builder \
+    python3-pip \
+    rustup \
+    libglib2.0-dev \
+    libostree-dev \
+    ;
+
+apt clean -y
+
+pip install --break-system-packages \
+    flit \
+    mypy \
+    packaging \
+    requirements-parser \
+    ruff \
+    tomlkit \
+    ;
+
+rustup default stable
+
+cargo install --git https://github.com/flatpak/flat-manager flat-manager-client
+
+flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
