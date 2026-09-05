49db5e572e953f3637d70339d6f2119d713bea8a
Dylan Baker <dylan@pnwbakers.com>
2026-09-05 18:40:49 +0000

    devcontainer: Further simplify with use of features

--- .devcontainer/debian-install.sh
+++ .devcontainer/debian-install.sh
@@ -1,21 +1,8 @@
 set -exo pipefail
 
-apt update -y
-
-apt install -y \
-    python3 \
-    appstream \
-    flatpak-builder \
-    python3-pip \
-    ;
-
-apt clean -y
-
 pip install --break-system-packages \
     flit \
     mypy \
-    packaging \
-    requirements-parser \
     ruff \
     tomlkit \
     pydantic \
